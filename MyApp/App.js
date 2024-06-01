fbbc85a3995a75d8c4c5aa5fb3c5da12bbcbdd36
ediscokes <edwardkoranteng2002@gmail.com>
2024-06-01 11:55:59 +0000

    'i created components and imported them in the App.js file'

---
+++ MyApp/App.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { View, Text, ScrollView, TextInput, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import TaskList from './components/TaskList';
+import Category from './components/Category';
